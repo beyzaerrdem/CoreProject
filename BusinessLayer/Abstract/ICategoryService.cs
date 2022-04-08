db3f29acba96c0d61c23282de3e4ebcf32d597be
beyzaerrdem <beyzardm502@gmail.com>
2022-04-08 14:47:26 +0300

    Add project files.

---
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICategoryService
+    {
+        void AddCategory(Category category);
+        void RemoveCategory(Category category);
+        void DeleteCategory(Category category);
+        List<Category> GetAllList();
+        Category GetById(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+       
+        public void AddCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAllList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
