13d3d7ddd0d7e29bb1a39bf7077037e7d8244f8e
beyzaerrdem <beyzardm502@gmail.com>
2022-04-11 03:20:46 +0300

    include method

---
+++ BusinessLayer/Abstract/IBlogService.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IBlogService
+    {
+        void BlogAdd(Blog blog);
+        void BlogUpdate(Blog blog); 
+        void BlogDelete(Blog blog);    
+        List<Blog> GetAllList();
+        Blog GetById(int id);
+
+        List<Blog> GetBlogListWithCategory();
+    }
+}
--- CoreProject/Controllers/BlogController.cs
+++ CoreProject/Controllers/BlogController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreProject.Controllers
 {
     public class BlogController : Controller
     {
+
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = bm.GetBlogListWithCategory();
+            return View(values);
         }
     }
 }
